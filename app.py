7757e04953f5d27dd3bc466ae2208d728ad010c5
Lesley Cordero <lc2958@columbia.edu>
2018-07-03 14:41:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from twilio.rest import Client
 class ADILabs:
     
     def __init__(self):
-        self.g = github.Github("your-username", "your-key")
-        self.client = Client(‘yourkey’, ‘your-key’)
-        self.index = 3 # index for ADILabs org
+        self.git_client = github.Github("username", "key-here")
+        self.twilio_client = Client('key-here', 'key-here')
+        self.index = 3
         self.pulls = []
         self.pull_counts = {}
         
-    def get_repos(self):
+    def get_all_repos(self):
+        self.pulls = []
         count = 0
-        for i in self.g.get_user().get_orgs()[self.index].get_repos():
+        for i in self.git_client.get_user().get_orgs()[self.index].get_repos():
             self.pulls.append(i.get_pulls('all'))
             self.pull_counts[count] = 0
             count += 1
@@ -25,26 +26,33 @@ class ADILabs:
             count += 1
     
     def check_counts(self):
-        self.set_pulls_list()
+	"""
+	checks current number of pull requests against last update of pull requests
+	returns: True or False (whether the number of pull requests is the same)
+	"""
         counts_check = {}
         count = 0 
-        for repo in self.pulls:
-            for j in repo:
+        for pl in self.git_client.get_user().get_orgs()[self.index].get_repos():
+            pl_repos = pl.get_pulls('all')
+            counts_check[count] = 0
+            for repo in pl_repos:
                 counts_check[count] = counts_check[count] + 1
             count += 1
         return self.pull_counts == counts_check
     
     def send_message(self):
-        message = self.client.messages.create(
-            to=""YOUR-NUMBER-HERE"", 
-            from_="YOUR-TWILIO-NUMBER-HERE",
+        message = self.twilio_client.messages.create(
+            to="+1number", # put numbers in +1XXXXXXXXXX format
+            from_="+1number", 
             body="You have new pull requests for ADI Labs!")
-	
-	
+
 if __name__ == "__main__":
     labs = ADILabs()
-    labs.get_repos()
-    labs.get_pull_counts()
+    labs.get_all_repos()
+    labs.set_pull_counts()
     while True:
         if not labs.check_counts():
             labs.send_message()
+            labs.pull_counts = {}
+            labs.get_all_repos()
+            labs.set_pull_counts()
